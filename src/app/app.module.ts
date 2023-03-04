0d01ca3414f10898ff11ca56acc02fc3d6fca13c
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-03-05 01:12:16 +0530

    bring in a couple fonts and add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
