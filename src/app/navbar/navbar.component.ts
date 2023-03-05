d71e4ff0a31572caa94049e7b54c77cd4052c0c9
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-03-05 21:54:23 +0530

    add navbar animations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,33 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  stagger,
+  transition,
+  query,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('navbarFade', [
+      transition('void => *', [
+        query('a', [
+          style({ opacity: 0, transform: 'translateY(20%)' }),
+          stagger(400, [
+            animate(
+              '0.5s 4s ease-out',
+              style({ opacity: 1, transform: 'translateY(0)' })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent implements AfterViewInit {
   constructor(private navbar: ElementRef<HTMLElement>) {}
